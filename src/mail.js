ea2d2050b0f5530592c4cb06474224d65a9755a6
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-11-15 10:11:02 +0900

    メール送信処理仮実装

---
+++ src/mail.js
@@ -0,0 +1,26 @@
+const mailer = require('nodemailer');
+
+const smtp = mailer.createTransport({
+  host: '127.0.0.1',
+  port: '1025',
+  auth: {
+    user: 'user',
+    pass: 'password',
+  }
+});
+
+const mailOptions = {
+  from: 'hoge@github.com',
+  to: 'hogehoge@github.com',
+  subject: 'メールタイトル',
+  html: 'メール本文です',
+};
+
+smtp.sendMail(mailOptions, function(err, info) {
+  if (!err) {
+    console.log('Mail success: ' + info.response);
+  } else {
+    console.log('Mail err', err);
+  }
+  smtp.close();
+});
